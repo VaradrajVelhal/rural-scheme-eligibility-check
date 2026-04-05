2fbff6178b7281e304ea0d2a8b4d8168b5a0aeb9
Varadraj Velhal <varadrajvelhal@Varadrajs-MacBook-Air.local>
2026-04-05 15:25:00 +0530

    Fix database config for Render

--- rural_scheme_engine/settings.py
+++ rural_scheme_engine/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -79,14 +80,7 @@ WSGI_APPLICATION = 'rural_scheme_engine.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
-    }
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 
